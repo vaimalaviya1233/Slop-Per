bf01a754cc07c65d4363c01c3ad2516a59a9e643
vaimalaviya1233 <vaibhavmalaviya@yahoo.com>
2021-04-08 23:18:49 -0500

    permissions

--- app/src/main/java/com/example/slop_per/Dloader.java
+++ app/src/main/java/com/example/slop_per/Dloader.java
@@ -1,9 +1,11 @@
 package com.example.slop_per;
 
+import android.app.DownloadManager;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.FileInputStream;
+import java.io.InputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -12,20 +14,30 @@ import java.io.*;
 import java.util.*;
 import java.nio.file.Path;
 
-
-public class Dloader {
-    public String dloader() {
+import static android.content.Context.DOWNLOAD_SERVICE;
+class Dloader{
+    /*public String dloader() {
         String result ="Good";
         try{
             URL url = new URL("https://tinyurl.com/38eruapu");
             FileOutputStream out = new FileOutputStream(new File("Vanilla.zip"));
-            out.write(url.openStream().read
+            InputStream inner = url.openStream();
+            out.write(inner.read());
+            out.close();
+            inner.close();
+
         }catch (MalformedURLException e){result = "Url address Wrong";}
         catch (IOException e){result ="File System Error";}
         
-        return "";
+        return result;
     }
+    private String download(android.net.Uri uri, String toStorage){
+        String result =" Status Fine ";
+        //DownloadManager dmanager =(DownloadManager)getSystemService(DOWNLOAD_SERVICE);
 
+        return result;
+    }
+*/
 }
 
 //Todo URL url = new URL("https://tinyurl.com/38eruapu");
--- app/src/main/java/com/example/slop_per/MainActivity.java
+++ app/src/main/java/com/example/slop_per/MainActivity.java
@@ -1,11 +1,14 @@
 package com.example.slop_per;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.content.pm.PackageManager;
 import android.*;
 import android.os.*;
 
 import android.widget.Button;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +16,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        android.widget.Button btnStart = findViewById(com.example.slop_per.R.id.starter);
-        btnStart.setOnClickListener(new android.view.View.OnClickListener() {
+
+        android.widget.Button button = findViewById(R.id.starter);
+        button.setOnClickListener(new android.view.View.OnClickListener() {
             @Override
             public void onClick(android.view.View v) {
-
+                /*if (checkSelfPermission(com.example.slop_per.Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED) {
+                    String[] permisissions = {Manifest.permission.WRITE_EXTERNAL_STORAGE};
+                } else {//granted permissions
+                    startDownload();
+                }*/
             }
         });
+    }
+
+    private void startDownload() {
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @androidx.annotation.NonNull String[] permissions, @androidx.annotation.NonNull int[] grantResults) {
+
     }
 }
